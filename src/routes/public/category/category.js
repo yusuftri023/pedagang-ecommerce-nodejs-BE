8da3c8d29db24edf08a2160592a8518ed439a065
yusuf <yusuftri023@gmail.com>
2024-08-29 15:05:22 +0800

    fix: order api endpoint integration

--- src/routes/public/category/category.js
+++ src/routes/public/category/category.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { allCategory } from "../../../controllers/category";
+import { allCategory } from "../../../controllers/category.js";
 
 const router = express.Router();
 
