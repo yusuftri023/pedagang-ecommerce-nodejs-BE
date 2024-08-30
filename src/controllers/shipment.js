69d1071432999188b96251cf8c397e0b9598da4c
yusuf <yusuftri023@gmail.com>
2024-08-30 21:55:36 +0800

    fix: 3rd party api for shipment integrated

--- src/controllers/shipment.js
+++ src/controllers/shipment.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { shipmentDetail } from "../models/shipment.js";
 
 export const customerShipment = async (req, res) => {
@@ -18,6 +19,50 @@ export const customerShipment = async (req, res) => {
         data: null,
       });
     }
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+      data: null,
+    });
+  }
+};
+
+export const province = async (req, res) => {
+  try {
+    const result = await axios
+      .get("https://api.rajaongkir.com/starter/province", {
+        headers: {
+          key: "fc047c1ff364a797b715979366c0c960",
+        },
+      })
+      .then((res) => res.data.rajaongkir.results);
+
+    return res
+      .status(200)
+      .json({ success: true, message: "success", data: result });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+      data: null,
+    });
+  }
+};
+export const city = async (req, res) => {
+  try {
+    const { province_id } = req.query;
+    const result = await axios
+      .get(`https://api.rajaongkir.com/starter/city?province=${province_id}`, {
+        headers: {
+          key: "fc047c1ff364a797b715979366c0c960",
+        },
+      })
+      .then((res) => res.data.rajaongkir.results);
+
+    return res
+      .status(200)
+      .json({ success: true, message: "success", data: result });
   } catch (error) {
     return res.status(500).json({
       success: false,
--- src/routes/public/shipment/shipment.js
+++ src/routes/public/shipment/shipment.js
@@ -1,16 +1,7 @@
 import express from "express";
-import axios from "axios";
+import { city, province } from "../../../controllers/shipment.js";
+
 const router = express.Router();
-router.get("/province", (req, res) => {
-  axios
-    .get("https://api.rajaongkir.com/starter/province", {
-      headers: {
-        key: "fc047c1ff364a797b715979366c0c960",
-      },
-      withCredentials: true,
-    })
-    .then((res) => console.log(res.data))
-    .catch((err) => console.log(err.response.data));
-  res.send("Testing");
-});
+router.get("/province", province);
+router.get("/city", city);
 export default router;
